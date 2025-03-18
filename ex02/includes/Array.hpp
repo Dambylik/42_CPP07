fceaf8f03d53c042c39e55254b3dc1de3d846c37
Dambulik home linux <dambylik@gmail.com>
2025-03-18 19:11:42 +0100

    ex02

--- ex02/includes/Array.hpp
+++ ex02/includes/Array.hpp
@@ -1,13 +1,90 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Iter.hpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: okapshai <okapshai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 14:21:42 by okapshai          #+#    #+#             */
-/*   Updated: 2025/03/18 14:45:21 by okapshai         ###   ########.fr       */
+/*   Updated: 2025/03/18 19:09:35 by okapshai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <iostream>
+#include <exception>
+#include <cstdlib>
+
+template<typename T>
+
+class Array
+{
+    public:
+        // constructor with unsigned int
+        Array(unsigned int n) {
+            tabSize = n;
+            tab = new T[n];
+
+            for(unsigned int i = 0; i < n; i++) {
+                tab[i] = 0;
+            }
+        }
+        
+        // construction by copy
+        Array(const Array& rhs) {
+
+            this->tabSize = rhs.size();
+            this->tab = new T[rhs.size()];
+
+            for(unsigned int i = 0; i < rhs.size(); i++) {
+                this->tab[i] = rhs[i]; //good si object bicoz appelle surcharge de l'operateur = 
+            }
+        }
+
+        // assignment operator
+        Array& operator=(const Array& rhs) {
+            delete [] tab; //very deep copy
+            tabSize = rhs.size();
+            tab = new T[rhs.size()]; // new template created with its own memory space
+
+            for (unsigned int i = 0; i < rhs.size(); i++) {
+                tab[i] = rhs[i];
+            }
+
+            return (*this);
+        }
+
+        // destructor
+        ~Array() {
+            delete [] tab;
+        }
+
+        // methods
+        //unsigned int, if a negative number is sent it will automatically overflow
+        T& operator[](unsigned int index) {
+            if (index >= this->tabSize) 
+                throw std::out_of_range("Out of range exception");
+            return (this->tab[index]);
+        }
+
+        const T& operator[](unsigned int index) const {
+            if (index >= this->tabSize)
+                throw std::out_of_range("Out of range exception");
+            return (this->tab[index]);
+        }
+        
+        unsigned int size() const {
+            return (this->tabSize);
+        }
+
+    private:
+        // constructor with no parameters
+        T* tab; 
+        unsigned int tabSize;
+
+        Array() {
+            tabSize = 0;
+            tab = new T[0];
+        }
+};
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,24 +6,117 @@
 /*   By: okapshai <okapshai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 12:26:03 by okapshai          #+#    #+#             */
-/*   Updated: 2025/03/18 14:41:44 by okapshai         ###   ########.fr       */
+/*   Updated: 2025/03/18 19:10:29 by okapshai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Iter.hpp"
+#include <iostream>
+#include "Array.hpp"
 
-int main(void) {
-    int tab[] = {1, 2, 33, 44};
+#define MAX_VAL 750
 
-    ::iter(tab, 4, print);
+int main(void)
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand() % 10;
+        numbers[i] = value;
+        mirror[i] = value;
+    }
 
-    std::string names[] = {
-        "riri",
-        "fifi",
-        "loulou"
-    };
+    //Test max value Array vs int[]
+    // for (int i = 0; i < MAX_VAL; i++)
+    // {
+    //     std::cout << "Numbers:"<< numbers[i] << std::endl;
+    //     std::cout << "Mirror:"<< mirror[i] << std::endl;
+    // }
+
+    //Scope Deep Copy test
+    std::cout << "\n------------------------------> Deep Copy tests" << std::endl;
+
+    {
+
+        std::cout << "\nOperator overload =" << std::endl;
+
+        Array<int> toCopy(10);
+
+        for (unsigned int i = 0; i < toCopy.size(); i++) {
+            toCopy[i] = i;
+        }
+
+        Array<int> copy = toCopy;
+
+        std::cout << "\nDisplay both arrays" << std::endl;
+		
+        for (unsigned int i = 0; i < toCopy.size(); i++) {
+            std::cout << "toCopy[" << i << "]: " << toCopy[i] << std::endl;
+            std::cout << "  copy[" << i << "]: " << copy[i] << std::endl;
+            std::cout << "\n";
+        }
+
+        std::cout << "\nModify \"copy\", shouldn't modify \"toCopy\"" << std::endl;
+        for (unsigned int i = 0; i < toCopy.size(); i++) {
+            copy[i] = 10 - i;
+        }
+
+        std::cout << "\"copy\" has been modified, print both arrays\n" << std::endl;
+        for (unsigned int i = 0; i < toCopy.size(); i++) {
+            std::cout << "toCopy[" << i << "]: " << toCopy[i] << std::endl;
+            std::cout << "  copy[" << i << "]: " << copy[i] << std::endl;
+            std::cout << "\n";
+        }
+
+		std::cout << "\n";
+        std::cout << "----------------> Copy through constructor test\n" << std::endl;
+
+        Array<int> copy2(copy);
+
+        for (unsigned int i = 0; i < copy.size(); i++) {
+            std::cout << " copy[" << i << "]: " << copy[i] << std::endl;
+            std::cout << "copy2[" << i << "]: " << copy2[i] << std::endl;
+		    std::cout << "\n";
+        }
+
+        std::cout << "\nModify \"copy2\", shouldn't modify \"copy\"" << std::endl;
+
+        for (unsigned int i = 0; i < toCopy.size(); i++) {
+            copy2[i] = i;
+        }
+
+        std::cout << "\"copy2\" has been modified, print both arrays\n" << std::endl;
+
+        for (unsigned int i = 0; i < copy.size(); i++) {
+            std::cout << " copy[" << i << "]: " << copy[i] << std::endl;
+            std::cout << "copy2[" << i << "]: " << copy2[i] << std::endl;
+		    std::cout << "\n";
+        }
+    }
+
+    for (int i = 0; i < MAX_VAL; i++) {
+        if (mirror[i] != numbers[i]) {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
     
-    ::iter(names, 3, print);
-    
-    return (0);
+	std::cout << "\n";
+    std::cout << "Quelques tests de l'index out of bound" << std::endl;
+
+    try {
+        numbers[-2] = 0;
+	} catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+    }
+
+    try {
+        numbers[MAX_VAL] = 0;
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+    }
+
+    delete [] mirror;//
+    return 0;
 }
