e99edb764881c328ca155fb8fa9f905f8c618298
okapshai <okapshai@student.42.fr>
2025-03-19 14:07:22 +0100

    ex01

--- ex01/includes/Iter.hpp
+++ ex01/includes/Iter.hpp
@@ -6,30 +6,25 @@
 /*   By: okapshai <okapshai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 14:21:42 by okapshai          #+#    #+#             */
-/*   Updated: 2025/03/18 14:42:10 by okapshai         ###   ########.fr       */
+/*   Updated: 2025/03/19 13:53:08 by okapshai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include "Colors.hpp"
 #include <iostream>
 #include <string>
 
-template<typename T>
+template< typename T >
 
-void iter(T* array, size_t length, void(*f)(const T&)) {
+void iter(T* array, size_t length, void(*func)( T & )) {
 
-    if (array) {
-        for (size_t i = 0; i < length; i++)
-            f(array[i]);
+    if (!array || !func) {
+        std::cout << "Error occured" << std::endl;
+        return;
     }
-    else
-        std::cout << "Something's not adding up here..." << std::endl;
-}
-
-//no need for protection because reference and T* not T**
-template<typename T>
-
-void print(const T& c) {
-    std::cout << c << std::endl;
+    for (size_t i = 0; i < length; i++) {
+        func(array[i]);
+    }
 }
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,24 +6,45 @@
 /*   By: okapshai <okapshai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 12:26:03 by okapshai          #+#    #+#             */
-/*   Updated: 2025/03/18 14:41:44 by okapshai         ###   ########.fr       */
+/*   Updated: 2025/03/19 14:07:03 by okapshai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Iter.hpp"
 
-int main(void) {
-    int tab[] = {1, 2, 33, 44};
+template <typename T>
 
-    ::iter(tab, 4, print);
-
-    std::string names[] = {
-        "riri",
-        "fifi",
-        "loulou"
-    };
-    
-    ::iter(names, 3, print);
-    
-    return (0);
+void printElement(T & elem) {
+    std::cout << elem << " ";
 }
+
+void doubleValue(int & n) {
+    n *= 10;
+}
+
+int main() {
+
+	std::cout << YELLOW << "\n------------Test with INTs------------\n" << RESET << std::endl;
+
+    int intArr[] = {1, 2, 3, 4, 5};
+    size_t intSize = sizeof(intArr) / sizeof(intArr[0]);
+
+    std::cout << MAGENTA << "Original int array: " << RESET;
+    iter(intArr, intSize, printElement<int>);
+    std::cout << std::endl;
+
+    std::cout << MAGENTA << "Multiplied int array: " << RESET;
+    iter(intArr, intSize, doubleValue);
+    iter(intArr, intSize, printElement<int>);
+    std::cout << std::endl;
+
+    std::cout << YELLOW << "\n------------Test with Strings------------\n" << RESET << std::endl;
+    std::string strArr[] = {"Hello", "Wonderful", "World"};
+    size_t strSize = sizeof(strArr) / sizeof(strArr[0]);
+
+    std::cout << MAGENTA << "String array: " << RESET;
+    iter(strArr, strSize, printElement<std::string>);
+    std::cout << std::endl << std::endl;
+    return 0;
+}
+
